e5faceea5e77b2de5cd875a34b82c6becd1aa467
Soulou <leo@unbekandt.eu>
2014-10-02 14:02:22 +0200

    Default version to v0.1.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 
 default["acadock"] = {
   "download_url" => "https://github.com/Soulou/acadock-monitoring/releases/download/",
-  "version" => "v0.1.0",
+  "version" => "v0.1.1",
   "arch" => "amd64",
   "install_path" => "/usr/local/bin",
   "port" => "4244"
