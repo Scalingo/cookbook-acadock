956b93c890d4fa2562a8f2b8672a04730b4dd1a7
Soulou <leo@unbekandt.eu>
2016-01-15 14:18:09 +0100

    Default version 0.3.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 
 default["acadock"] = {
   "download_url" => "https://github.com/Scalingo/acadock-monitoring/releases/download",
-  "version" => "0.3.1",
+  "version" => "0.3.2",
   "arch" => "amd64",
   "install_path" => "/usr/local/bin",
   "port" => "4244"
