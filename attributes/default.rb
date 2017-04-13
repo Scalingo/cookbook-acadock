0a3c94fd6b986d3ef22d9ac61e0c5595260a6a56
Soulou <leo@unbekandt.eu>
2017-04-13 18:14:12 +0200

    Default version to 0.4.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 
 default["acadock"] = {
   "download_url" => "https://github.com/Scalingo/acadock-monitoring/releases/download",
-  "version" => "0.3.4",
+  "version" => "0.4.0",
   "arch" => "amd64",
   "install_path" => "/usr/local/bin",
   "docker_url" => "http://127.0.0.1:4243",
