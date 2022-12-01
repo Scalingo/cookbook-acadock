dd6ce802e89f914ab0a48a0382b48d36b4198300
Brandon WELSCH <24525868+brandon-welsch@users.noreply.github.com>
2022-12-01 12:50:25 +0100

    release: Use v1.2.0 by default

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'leo@scalingo.com'
 license          'MIT'
 description      'Installs/Configures acadock monitoring tool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.2.0'
